5593af8c6c461e7ff2f14c751a53b2edf43dc0a8
Ben Massar <bemmacintosh@gmail.com>
2014-12-15 20:50:02 -0500

    First work on the program. Start on the image class.

---
+++ src/robot/vision/base/Image.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robot.vision.base;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author FantasticNinjas
+ */
+public class Image {
+    
+    private Color[] pixels;
+    private int width;
+    private int height;
+    
+    public Image(Color[] pixels, int width, int height) {
+        
+    }
+    
+}
